64877618d35c754204ad98266fff354e03c1ebba
Jonathan Camilleri <xmiq@live.com>
2016-09-26 06:34:12 +0200

    Expanded the Skill Class to include every field in the database table

--- Manager/Models/Skill.cs
+++ Manager/Models/Skill.cs
@@ -13,5 +13,26 @@ namespace Manager.Models
 
         [Required]
         public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public SkillType Type { get; set; }
+
+        [Required, Display(Name = "Active Description Formula")]
+        public string ActiveDescriptionFormula { get; set; }
+
+        [Required, Display(Name = "Passive Description Formula")]
+        public string PassiveDescriptionFormula { get; set; }
+
+        [Required, Display(Name = "Active Formula")]
+        public string ActiveFormula { get; set; }
+
+        [Required, Display(Name = "Active Cost Formula")]
+        public string ActiveCostFormula { get; set; }
+
+        [Required, Display(Name = "Passive Formula")]
+        public string PassiveFormula { get; set; }
     }
 }
