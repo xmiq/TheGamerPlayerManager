8ac87c228ffa0c76a9f9810eb012c7bb9b2db7ec
Jonathan Camilleri <xmiq@live.com>
2016-10-31 07:30:22 +0100

    Completed the Manager Class

--- Manager/DataManagement/Manager.cs
+++ Manager/DataManagement/Manager.cs
@@ -9,17 +9,29 @@ namespace Manager.DataManagement
     {
         public Manager(ManagerClasses toInitialize)
         {
+            if (toInitialize.HasFlag(ManagerClasses.Chapter)) Chapter = new ChapterManager();
             if (toInitialize.HasFlag(ManagerClasses.Player)) Player = new PlayerManager();
+            if (toInitialize.HasFlag(ManagerClasses.Skill)) Skill = new SkillManager();
+            if (toInitialize.HasFlag(ManagerClasses.SkillStats)) SkillStats = new SkillStatsManager();
+            if (toInitialize.HasFlag(ManagerClasses.Stats)) Stats = new StatsManager();
             if (toInitialize.HasFlag(ManagerClasses.Story)) Story = new StoryManager();
             if (toInitialize.HasFlag(ManagerClasses.User)) User = new UserManager();
         }
 
+        public ChapterManager Chapter { get; set; }
+
         public PlayerManager Player { get; set; }
+
+        public SkillManager Skill { get; set; }
+
+        public SkillStatsManager SkillStats { get; set; }
+
+        public StatsManager Stats { get; set; }
 
         public StoryManager Story { get; set; }
 
         public UserManager User { get; set; }
     }
 
-    public enum ManagerClasses { Player = 2, Story = 32, User = 64 }
+    public enum ManagerClasses { Chapter = 1, Player = 2, Skill = 4, SkillStats = 8, Stats = 16, Story = 32, User = 64 }
 }
